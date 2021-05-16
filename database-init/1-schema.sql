7b959ccf4d1a0a08a510b161e4596aa29bbc9579
AzisK <azuolas.krusna@yahoo.com>
2021-05-16 16:53:36 +0300

    Add user, user_active_count and user_event tables

--- database-init/1-schema.sql
+++ database-init/1-schema.sql
@@ -1,7 +1,9 @@
+-- Organization
+
 CREATE TABLE organization (
     id VARCHAR(255) PRIMARY KEY,
-    name VARCHAR(255),
-    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    name VARCHAR(255) NOT NULL,
+    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
@@ -9,6 +11,49 @@ CREATE EXTENSION moddatetime;
 
 CREATE TRIGGER organization_modified BEFORE UPDATE ON organization
 FOR EACH ROW EXECUTE PROCEDURE moddatetime (modified);
+
+-- User
+
+CREATE TABLE user (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    type VARCHAR(255) NOT NULL,
+    organization_name VARCHAR(255) REFERENCES organization(name) ON DELETE CASCADE NOT NULL,
+    plan_name VARCAHR(255) NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    current BOOLEAN NOT NULL,
+    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+-- User
+
+CREATE TABLE user_active_count (
+    date DATE PRIMARY KEY,
+    count VARCHAR(255) NOT NULL,
+    type VARCHAR(255) NOT NULL,
+    organization_name VARCHAR(255) REFERENCES organization(name) ON DELETE CASCADE NOT NULL,
+    plan_name VARCAHR(255) NOT NULL,
+    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+-- User events
+
+CREATE TABLE user_event (
+    id VARCHAR(255) PRIMARY KEY,
+    event_type VARCHAR(255) NOT NULL,
+    user_name VARCHAR(255) NOT NULL,
+    user_email VARCHAR(255) NOT NULL,
+    user_type VARCHAR(255) NOT NULL,
+    organization_name VARCHAR(255) REFERENCES organization(name) ON DELETE CASCADE NOT NULL,
+    plan_name VARCAHR(255) NOT NULL,
+    received_at TIMESTAMP NOT NULL,
+    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
 
 -- Table initialization by Python status
 
